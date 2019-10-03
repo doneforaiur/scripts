1db11b004ba4b132ad12be949814a462fc3086c9
Mirza Atlı <mirza.a.96@hotmail.com>
2019-10-03 11:09:10 +0300

    dijital.sh

---
+++ dijital.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Launch gedit, okular and start re-compiling tex file whenever .tex file is updated.
+
+
+xdotool key 'alt+shift+0'
+okular $1.pdf & gedit $1.tex &
+ls *.tex | entr pdflatex $1.tex
+
+
