b00631678a57ae0fe1c8839a4c2ef819aa9c7d8b
Mirza Atlı <mirza.a.96@hotmail.com>
2019-10-11 13:43:32 +0300

    create new .tex and .pdf files

--- dijital.sh
+++ dijital.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 # Launch gedit, okular and start re-compiling tex file whenever .tex file is updated.
 
-
-xdotool key 'alt+shift+0'
-okular $1.pdf & gedit $1.tex &
-ls *.tex | entr pdflatex $1.tex
-
+if [ -z "$1" ]
+then
+	echo "You have to specify which file to process."
+else
+	if [ -e "$1.tex" ] && [ -e "$1.pdf" ]
+	then
+		xdotool key 'alt+shift+0' &
+		gedit $1.tex &
+		okular $1.pdf &
+		ls *.tex | entr pdflatex $1.tex
+	else
+		echo "Either .PDF or .TEX files are missing."
+		echo "Create? [Y/n]"
+		read user_input
+		if [ $user_input == 'Y' ]
+		then
+			touch $1.tex $1.pdf & 
+			xdotool key 'alt+shift+0' &
+			gedit $1.tex &
+			convert xc:none -page A4 $1.pdf &
+			okular $1.pdf &
+			ls *.tex | entr pdflatex $1.tex
+		else
+			exit 0
+		fi
+	fi
+	
+fi
 
